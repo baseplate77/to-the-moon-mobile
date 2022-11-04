491717dcfabc5b1ad1bc44fe1f3d60064e9a2836
hrushikesh <kukularehrushi@gmail.com>
2022-11-04 11:46:49 +0530

    Renamed dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-
-import Game from "./Compoment/Game/Game"
-
+import Game from "./components/Game/Game";
 
 function App() {
-
-
   return (
-   <div className="w-screen h-screen">
-    <Game />
-   </div> 
+    <div className="w-screen h-screen">
+      <Game />
+    </div>
   );
 }
 
