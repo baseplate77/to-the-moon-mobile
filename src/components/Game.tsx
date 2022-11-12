47706d87678a40883b39883fa444dd238a6a70df
baseplate77 <base8087@gmail.com>
2022-11-12 16:42:42 +0530

    restart callback added

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -6,38 +6,55 @@ import "../styles/game.css";
 type GameProps = {};
 
 function Game({}: GameProps) {
-const [score, setScore ] = useState(0);
+  const [score, setScore] = useState(0);
+  const [gameOver ,setGameOver] = useState(false);
 
-  const { unityProvider, isLoaded, loadingProgression, addEventListener,removeEventListener } = useUnityContext({
+  const {
+    unityProvider,
+    isLoaded,
+    loadingProgression,
+    sendMessage,
+    addEventListener,
+    removeEventListener,
+  } = useUnityContext({
     loaderUrl: "Build/webBuild.loader.js",
     dataUrl: "Build/webBuild.data",
     frameworkUrl: "Build/webBuild.framework.js",
     codeUrl: "Build/webBuild.wasm",
   });
- const handleGameOver = useCallback((score:any) => {
-    setScore(score);
-  }, []);
-  const loadingPercentage = Math.round(loadingProgression * 100);
 
+  const handleGameOver = useCallback((score: any) => {
+    setScore(score);
+    setGameOver(true);
+  }, []);
+
+  const handleRestart = () => {
+    sendMessage("GameManager", "Restart");
+    setGameOver(false);
+  };
+
+  const loadingPercentage = Math.round(loadingProgression * 100);
+  
 
   useEffect(() => {
     addEventListener("GameOver", handleGameOver);
     return () => {
       removeEventListener("GameOver", handleGameOver);
     };
-  },[addEventListener,removeEventListener,handleGameOver])
+  }, [addEventListener, removeEventListener, handleGameOver]);
 
-  console.log("score",score);
-  
+  console.log("score", score);
 
   return (
-    <div className="container">
+    <div className="container ">
       {isLoaded === false && (
         <div className="loading-overlay">
           <p>Loading... ({loadingPercentage}%)</p>
         </div>
       )}
+
       <Unity className="unity" unityProvider={unityProvider} />
+      <button onClick={handleRestart}>Restart </button>
     </div>
   );
 }
