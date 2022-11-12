a53ed97258518ff7a6c4fde4a0b382812cb552f1
baseplate77 <base8087@gmail.com>
2022-11-12 15:45:26 +0530

    game update

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,17 +1,34 @@
+import { log } from "console";
+import { useCallback, useEffect, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import "../styles/game.css";
 
 type GameProps = {};
 
 function Game({}: GameProps) {
-  const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
+const [score, setScore ] = useState(0);
+
+  const { unityProvider, isLoaded, loadingProgression, addEventListener,removeEventListener } = useUnityContext({
     loaderUrl: "Build/webBuild.loader.js",
     dataUrl: "Build/webBuild.data",
     frameworkUrl: "Build/webBuild.framework.js",
     codeUrl: "Build/webBuild.wasm",
   });
-
+ const handleGameOver = useCallback((score:any) => {
+    setScore(score);
+  }, []);
   const loadingPercentage = Math.round(loadingProgression * 100);
+
+
+  useEffect(() => {
+    addEventListener("GameOver", handleGameOver);
+    return () => {
+      removeEventListener("GameOver", handleGameOver);
+    };
+  },[addEventListener,removeEventListener,handleGameOver])
+
+  console.log("score",score);
+  
 
   return (
     <div className="container">
